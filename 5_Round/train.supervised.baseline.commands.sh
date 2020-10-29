fedb57e954b30f8876cd7575515f7416bc03404a
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-10-29 08:44:33 -0700

    coding

--- 5_Round/train.supervised.baseline.commands.sh
+++ 5_Round/train.supervised.baseline.commands.sh
@@ -1,6 +1,6 @@
-export SHOT=10 #1, 3, 5, 10, 100000
 export BATCHSIZE=20
 export EPOCHSIZE=30
+export SEED=42 #42, 16, 32, 64, 128
 export LEARNINGRATE=1e-6
 
 
@@ -11,11 +11,11 @@ CUDA_VISIBLE_DEVICES=0 python -u train.supervised.baseline.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 42 \
-    --round_name base > log.base.txt 2>&1 &
+    --seed $SEED \
+    --round_name base > log.supervised.base.seed.$SEED.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=1 python -u train.supervised.baseline.py \
     --task_name rte \
@@ -23,11 +23,11 @@ CUDA_VISIBLE_DEVICES=1 python -u train.supervised.baseline.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 16 \
-    --round_name r1 > log.r1.txt 2>&1 &
+    --seed $SEED \
+    --round_name r1 > log.supervised.r1.seed.$SEED.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=2 python -u train.supervised.baseline.py \
     --task_name rte \
@@ -35,11 +35,11 @@ CUDA_VISIBLE_DEVICES=2 python -u train.supervised.baseline.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 32 \
-    --round_name r2 > log.r2.txt 2>&1 &
+    --seed $SEED \
+    --round_name r2 > log.supervised.r2.seed.$SEED.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=3 python -u train.supervised.baseline.py \
     --task_name rte \
@@ -47,24 +47,24 @@ CUDA_VISIBLE_DEVICES=3 python -u train.supervised.baseline.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 64 \
-    --round_name r3 > log.r3.txt 2>&1 &
+    --seed $SEED \
+    --round_name r3 > log.supervised.r3.seed.$SEED.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=6 python -u train.supervised.baseline.py \
+CUDA_VISIBLE_DEVICES=4 python -u train.supervised.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 128 \
-    --round_name r4 > log.r4.txt 2>&1 &
+    --seed $SEED \
+    --round_name r4 > log.supervised.r4.seed.$SEED.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=5 python -u train.supervised.baseline.py \
     --task_name rte \
@@ -72,8 +72,8 @@ CUDA_VISIBLE_DEVICES=5 python -u train.supervised.baseline.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 128 \
-    --round_name r5 > log.r5.txt 2>&1 &
+    --seed $SEED \
+    --round_name r5 > log.supervised.r5.seed.$SEED.txt 2>&1 &
