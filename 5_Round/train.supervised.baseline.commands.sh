5c89e7062cd521c4499f39cbf4cb61a8f4600e8e
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-10-29 19:46:02 -0700

    coding

--- 5_Round/train.supervised.baseline.commands.sh
+++ 5_Round/train.supervised.baseline.commands.sh
@@ -1,6 +1,6 @@
 export BATCHSIZE=20
 export EPOCHSIZE=50
-export SEED=16 #42, 16, 32
+export SEED=32 #42, 16, 32
 export LEARNINGRATE=1e-6
 
 
@@ -66,7 +66,7 @@ CUDA_VISIBLE_DEVICES=4 python -u train.supervised.baseline.py \
     --seed $SEED \
     --round_name r4 > log.supervised.r4.seed.$SEED.debug.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=5 python -u train.supervised.baseline.py \
+CUDA_VISIBLE_DEVICES=6 python -u train.supervised.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
