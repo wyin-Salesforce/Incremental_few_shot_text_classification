cf706856f1cb73c9ae9668d6a38bae7f176f5ff1
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-10-30 07:11:02 -0700

    coding

--- 5_Round/compute_mean_std.py
+++ 5_Round/compute_mean_std.py
@@ -10,7 +10,7 @@ print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)
 
-print(str(average)+'/'+str(res))
+print(str(average)+'$\pm$'+str(res))
 
 '''
 67.93/3.31
