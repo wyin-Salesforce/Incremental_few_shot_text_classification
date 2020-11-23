0a753aa992224c6439fcc566e0952eb5202bcd8d
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-22 23:50:38 -0800

    coding

--- 5_Round/train.ours.entail.py
+++ 5_Round/train.ours.entail.py
@@ -658,36 +658,27 @@ def main():
                 '''compute loss decay'''
                 if round !='base':
                     '''fake pos'''
-                    decay=0.05
-                    # print('cosine_matrix:', cosine_matrix)
-                    # print('train_pair_type_ids:', train_pair_type_ids)
+                    decay=0.0
                     col_indices_regPos = (train_pair_type_ids==train_type_list.index('regPos')).nonzero(as_tuple=False).view(-1)
                     exacted_cosine_part = decay*cosine_matrix[:,col_indices_regPos]
                     if exacted_cosine_part.nelement() != 0:
                         decay_vec_fakePos = torch.mean(exacted_cosine_part,axis=1) #batch
                         decay_vec_fakePos[train_pair_type_ids!=train_type_list.index('fakePos')]=1.0
                     else:
-                        decay_vec_fakePos=torch.tensor([0.05]*input_ids.shape[0]).to(device)
-                        # decay_vec_fakePos.to(device)
+                        decay_vec_fakePos=torch.tensor([0.1]*input_ids.shape[0]).to(device)
                         decay_vec_fakePos[train_pair_type_ids!=train_type_list.index('fakePos')]=1.0
 
 
-                    # print('decay_vec_fakePos:', decay_vec_fakePos)
                     '''fake neg'''
                     col_indices_regNeg = (train_pair_type_ids==train_type_list.index('regNeg')).nonzero(as_tuple=False).view(-1)
-                    # print("train_type_list.index('regNeg'):", train_type_list.index('regNeg'))
 
                     exacted_cosine_part = decay*cosine_matrix[:,col_indices_regNeg]
-                    # print('exacted_cosine_part:', exacted_cosine_part)
-                    # print('exacted_cosine_part.nelement():', exacted_cosine_part.nelement())
                     if exacted_cosine_part.nelement() != 0:
                         decay_vec_fakeNeg = torch.mean(exacted_cosine_part,axis=1) #batch
                         decay_vec_fakeNeg[train_pair_type_ids!=train_type_list.index('fakeNeg')]=1.0
                     else:
                         decay_vec_fakeNeg=torch.tensor([0.1]*input_ids.shape[0]).to(device)
-                        # decay_vec_fakeNeg.to(device)
                         decay_vec_fakeNeg[train_pair_type_ids!=train_type_list.index('fakeNeg')]=1.0
-                    # print('decay_vec_fakeNeg:', decay_vec_fakeNeg)
 
 
                 loss_fct = CrossEntropyLoss(reduction='none')
