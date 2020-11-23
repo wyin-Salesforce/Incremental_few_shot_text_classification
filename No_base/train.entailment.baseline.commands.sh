aa8eaf509386c2f3677aaf067e471053d44ead45
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-23 14:45:00 -0800

    coding

--- No_base/train.entailment.baseline.commands.sh
+++ No_base/train.entailment.baseline.commands.sh
@@ -1,6 +1,6 @@
 export BATCHSIZE=16
 export EPOCHSIZE=5
-export SEED=32 #42, 16, 32
+export SEED=42 #42, 16, 32
 export LEARNINGRATE=1e-6
 
 #running time: 10mins per epoch
@@ -15,7 +15,7 @@ CUDA_VISIBLE_DEVICES=0 python -u train.entailment.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name base > log.entailment.base.seed.$SEED.txt 2>&1 &
+    --round_name r1 > log.nobase.entailment.r1.seed.$SEED.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=1 python -u train.entailment.baseline.py \
     --task_name rte \
@@ -27,7 +27,7 @@ CUDA_VISIBLE_DEVICES=1 python -u train.entailment.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name r1 > log.entailment.r1.seed.$SEED.txt 2>&1 &
+    --round_name r2 > log.nobase.entailment.r2.seed.$SEED.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=2 python -u train.entailment.baseline.py \
     --task_name rte \
@@ -39,7 +39,8 @@ CUDA_VISIBLE_DEVICES=2 python -u train.entailment.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name r2 > log.entailment.r2.seed.$SEED.txt 2>&1 &
+    --round_name r3 > log.nobase.entailment.r3.seed.$SEED.txt 2>&1 &
+
 
 CUDA_VISIBLE_DEVICES=3 python -u train.entailment.baseline.py \
     --task_name rte \
@@ -51,8 +52,7 @@ CUDA_VISIBLE_DEVICES=3 python -u train.entailment.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name r3 > log.entailment.r3.seed.$SEED.txt 2>&1 &
-
+    --round_name r4 > log.nobase.entailment.r4.seed.$SEED.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=4 python -u train.entailment.baseline.py \
     --task_name rte \
@@ -64,16 +64,4 @@ CUDA_VISIBLE_DEVICES=4 python -u train.entailment.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name r4 > log.entailment.r4.seed.$SEED.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=5 python -u train.entailment.baseline.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
-    --seed $SEED \
-    --round_name r5 > log.entailment.r5.seed.$SEED.txt 2>&1 &
+    --round_name r5 > log.nobase.entailment.r5.seed.$SEED.txt 2>&1 &
--- No_base/train.entailment.baseline.py
+++ No_base/train.entailment.baseline.py
@@ -567,12 +567,11 @@ def main():
     if task_name not in processors:
         raise ValueError("Task not found: %s" % (task_name))
 
-    round_name_2_rounds={'base':['base', 'ood'],
-                         'r1':['base', 'n1', 'ood'],
-                         'r2':['base', 'n1', 'n2', 'ood'],
-                         'r3':['base', 'n1', 'n2', 'n3', 'ood'],
-                         'r4':['base', 'n1', 'n2', 'n3','n4', 'ood'],
-                         'r5':['base', 'n1', 'n2', 'n3','n4', 'n5', 'ood']}
+    round_name_2_rounds={'r1':['n1', 'ood'],
+                         'r2':['n1', 'n2', 'ood'],
+                         'r3':['n1', 'n2', 'n3', 'ood'],
+                         'r4':[ 'n1', 'n2', 'n3','n4', 'ood'],
+                         'r5':[ 'n1', 'n2', 'n3','n4', 'n5', 'ood']}
 
 
 
@@ -600,7 +599,7 @@ def main():
     '''load training in list'''
     train_examples_list, train_class_list, train_class_2_split_list = processor.load_train(round_list[:-1]) # no odd training examples
     assert len(train_class_list) == len(train_class_2_split_list)
-    assert len(train_class_list) ==  20+(len(round_list)-2)*10
+    # assert len(train_class_list) ==  20+(len(round_list)-2)*10
     '''dev and test'''
     dev_examples = processor.load_dev_or_test(round_list, train_class_list, 'dev')
     test_examples = processor.load_dev_or_test(round_list, train_class_list, 'test')
