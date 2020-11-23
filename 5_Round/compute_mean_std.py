ce4818851ab21f6b3b371fc863f1ce5617e9e82a
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-23 11:45:02 -0800

    coding

--- 5_Round/compute_mean_std.py
+++ 5_Round/compute_mean_std.py
@@ -22,9 +22,9 @@ def compute(test_list):
     return str(average)+'$\\pm$'+str(res)
 
 def extract(flag):
-    filenames = ['log.dynamicFewShot.'+flag+'.seed.42.debug.txt',
-                 'log.dynamicFewShot.'+flag+'.seed.16.debug.txt',
-                 'log.dynamicFewShot.'+flag+'.seed.32.debug.txt']
+    filenames = ['log.entail.v2.'+flag+'.seed.42.debug.txt',
+                 'log.entail.v2.'+flag+'.seed.16.debug.txt']
+                 # 'log.entail.v2.'+flag+'.seed.32.debug.txt']
     result_lists = []
     for fil in filenames:
         readfile = codecs.open('/export/home/workspace/Incremental_few_shot_text_classification/5_Round/'+fil, 'r', 'utf-8')
