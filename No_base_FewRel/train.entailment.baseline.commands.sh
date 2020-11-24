c2fd7e8b89a1499342114541828107fe878aba83
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-23 23:29:01 -0800

    coding

--- No_base_FewRel/train.entailment.baseline.commands.sh
+++ No_base_FewRel/train.entailment.baseline.commands.sh
@@ -1,6 +1,6 @@
 export BATCHSIZE=16
 export EPOCHSIZE=50
-export SEED=42 #42, 16, 32
+export SEED=16 #42, 16, 32
 export LEARNINGRATE=1e-6
 
 #running time: 10mins per epoch
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=0 python -u train.entailment.baseline.py \
     --seed $SEED \
     --round_name r1 > log.nobase.entailment.r1.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u train.entailment.baseline.py \
+CUDA_VISIBLE_DEVICES=5 python -u train.entailment.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -29,7 +29,7 @@ CUDA_VISIBLE_DEVICES=1 python -u train.entailment.baseline.py \
     --seed $SEED \
     --round_name r2 > log.nobase.entailment.r2.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python -u train.entailment.baseline.py \
+CUDA_VISIBLE_DEVICES=6 python -u train.entailment.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -42,7 +42,7 @@ CUDA_VISIBLE_DEVICES=2 python -u train.entailment.baseline.py \
     --round_name r3 > log.nobase.entailment.r3.seed.$SEED.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=3 python -u train.entailment.baseline.py \
+CUDA_VISIBLE_DEVICES=7 python -u train.entailment.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -54,7 +54,7 @@ CUDA_VISIBLE_DEVICES=3 python -u train.entailment.baseline.py \
     --seed $SEED \
     --round_name r4 > log.nobase.entailment.r4.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=4 python -u train.entailment.baseline.py \
+CUDA_VISIBLE_DEVICES=1 python -u train.entailment.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
