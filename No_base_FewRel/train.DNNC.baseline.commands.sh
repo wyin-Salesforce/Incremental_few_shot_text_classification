458ecc9d30fd8dc2734b4b482e9c3cd5a4e072ac
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-30 09:06:00 -0800

    final version after deadline

--- No_base_FewRel/train.DNNC.baseline.commands.sh
+++ No_base_FewRel/train.DNNC.baseline.commands.sh
@@ -1,11 +1,12 @@
-export BATCHSIZE=10
-export EPOCHSIZE=50 #50
-export SEED=42 #42, 16, 32
+export BATCHSIZE=16
+export EPOCHSIZE=50
+export SEED=32 #42, 16, 32
 export LEARNINGRATE=1e-6
 
-#running time:
+#running time: 45mins per epoch
 
-CUDA_VISIBLE_DEVICES=1 python -u train.protonet.baseline.py \
+
+CUDA_VISIBLE_DEVICES=1 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -13,11 +14,11 @@ CUDA_VISIBLE_DEVICES=1 python -u train.protonet.baseline.py \
     --train_batch_size $BATCHSIZE \
     --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
+    --max_seq_length 128 \
     --seed $SEED \
-    --round_name r1 > log.nobase.prototype.r1.seed.$SEED.txt 2>&1 &
+    --round_name r1 > log.nobase.DNNC.r1.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python -u train.protonet.baseline.py \
+CUDA_VISIBLE_DEVICES=2 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -25,11 +26,11 @@ CUDA_VISIBLE_DEVICES=2 python -u train.protonet.baseline.py \
     --train_batch_size $BATCHSIZE \
     --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
+    --max_seq_length 128 \
     --seed $SEED \
-    --round_name r2 > log.nobase.prototype.r2.seed.$SEED.txt 2>&1 &
+    --round_name r2 > log.nobase.DNNC.r2.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=3 python -u train.protonet.baseline.py \
+CUDA_VISIBLE_DEVICES=3 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -37,12 +38,12 @@ CUDA_VISIBLE_DEVICES=3 python -u train.protonet.baseline.py \
     --train_batch_size $BATCHSIZE \
     --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
+    --max_seq_length 128 \
     --seed $SEED \
-    --round_name r3 > log.nobase.prototype.r3.seed.$SEED.txt 2>&1 &
+    --round_name r3 > log.nobase.DNNC.r3.seed.$SEED.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=0 python -u train.protonet.baseline.py \
+CUDA_VISIBLE_DEVICES=4 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -50,11 +51,11 @@ CUDA_VISIBLE_DEVICES=0 python -u train.protonet.baseline.py \
     --train_batch_size $BATCHSIZE \
     --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
+    --max_seq_length 128 \
     --seed $SEED \
-    --round_name r4 > log.nobase.prototype.r4.seed.$SEED.txt 2>&1 &
+    --round_name r4 > log.nobase.DNNC.r4.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=5 python -u train.protonet.baseline.py \
+CUDA_VISIBLE_DEVICES=5 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -62,6 +63,6 @@ CUDA_VISIBLE_DEVICES=5 python -u train.protonet.baseline.py \
     --train_batch_size $BATCHSIZE \
     --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
+    --max_seq_length 128 \
     --seed $SEED \
-    --round_name r5 > log.nobase.prototype.r5.seed.$SEED.txt 2>&1 &
+    --round_name r5 > log.nobase.DNNC.r5.seed.$SEED.txt 2>&1 &
--- No_base_Intent/train.DNNC.baseline.commands.sh
+++ No_base_Intent/train.DNNC.baseline.commands.sh
@@ -1,11 +1,12 @@
 export BATCHSIZE=32
-export EPOCHSIZE=20
+export EPOCHSIZE=5
 export SEED=32 #42, 16, 32
 export LEARNINGRATE=1e-6
 
-#running time, 90mins per seed
+#running time: 45mins per epoch
 
-CUDA_VISIBLE_DEVICES=0 python -u train.dynamicFewShot.baseline.py \
+
+CUDA_VISIBLE_DEVICES=0 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -15,9 +16,9 @@ CUDA_VISIBLE_DEVICES=0 python -u train.dynamicFewShot.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name base > log.dynamicFewShot.base.seed.$SEED.debug.txt 2>&1 &
+    --round_name r1 > log.nobase.DNNC.r1.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u train.dynamicFewShot.baseline.py \
+CUDA_VISIBLE_DEVICES=1 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -27,9 +28,9 @@ CUDA_VISIBLE_DEVICES=1 python -u train.dynamicFewShot.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name r1 > log.dynamicFewShot.r1.seed.$SEED.debug.txt 2>&1 &
+    --round_name r2 > log.nobase.DNNC.r2.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python -u train.dynamicFewShot.baseline.py \
+CUDA_VISIBLE_DEVICES=2 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -39,9 +40,10 @@ CUDA_VISIBLE_DEVICES=2 python -u train.dynamicFewShot.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name r2 > log.dynamicFewShot.r2.seed.$SEED.debug.txt 2>&1 &
+    --round_name r3 > log.nobase.DNNC.r3.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=3 python -u train.dynamicFewShot.baseline.py \
+
+CUDA_VISIBLE_DEVICES=3 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -51,10 +53,9 @@ CUDA_VISIBLE_DEVICES=3 python -u train.dynamicFewShot.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name r3 > log.dynamicFewShot.r3.seed.$SEED.debug.txt 2>&1 &
+    --round_name r4 > log.nobase.DNNC.r4.seed.$SEED.txt 2>&1 &
 
-
-CUDA_VISIBLE_DEVICES=4 python -u train.dynamicFewShot.baseline.py \
+CUDA_VISIBLE_DEVICES=4 python -u train.DNNC.baseline.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -64,16 +65,4 @@ CUDA_VISIBLE_DEVICES=4 python -u train.dynamicFewShot.baseline.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name r4 > log.dynamicFewShot.r4.seed.$SEED.debug.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=5 python -u train.dynamicFewShot.baseline.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
-    --seed $SEED \
-    --round_name r5 > log.dynamicFewShot.r5.seed.$SEED.debug.txt 2>&1 &
+    --round_name r5 > log.nobase.DNNC.r5.seed.$SEED.txt 2>&1 &
